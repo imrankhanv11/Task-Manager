5e880db5c61ed7b4795dab9ee28ad7cae85cdbbf
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-06 17:19:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -184,11 +184,8 @@ $('document').ready(function () {
     $('#output').on('click', '.upbtn', async function (e) {
         e.preventDefault();
 
-        // getting id which one need to update
-        const id = $(this).attr('data-id');
 
-        // set
-        changeId = id;
+        changeId = $(this).attr('data-id');
 
         // this btn
         $(this).hide();
@@ -207,7 +204,7 @@ $('document').ready(function () {
         isDone.prop('checked', false);
 
         try {
-            const upResponse = await fetch(`${url}/${id}`);
+            const upResponse = await fetch(`${url}/${changeId}`);
             const data = await upResponse.json();
 
             // populate the details
@@ -358,7 +355,7 @@ $('document').ready(function () {
 
             let count = 0;
             data.forEach(element => {
-                if (element.title.startsWith(searchtit)) {
+                if (element.title.toLowerCase().startsWith(searchtit.toLowerCase())) {
                     output.append(displayFormat(element));
                     count++;
                 }
@@ -386,6 +383,45 @@ $('document').ready(function () {
         $(this).hide();
         displayTask();
     })
+
+    // ----------------SORTING---------------------
+    $('#btn-sort').on('click', async function (e) {
+        e.preventDefault();
+
+        let output = $('#output');
+        output.empty();
+
+        $('#searc-btn-sort-back').show();
+
+        try {
+            const sortResponse = await fetch(url);
+
+            if (!sortResponse.ok) {
+                throw new Error("Can't fetch the data");
+            }
+
+            const data = await sortResponse.json();
+
+            const datasort = data.sort((a, b) => b.priority - a.priority);
+
+            // foreach loop to seprate each task
+            datasort.forEach(Element => {
+                output.append(displayFormat(Element));
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+    })
+
+    // back -- for short
+        // back btn -- for title
+    $('#searc-btn-sort-back').on('click', function (e) {
+        e.preventDefault();
+        $(this).hide();
+        displayTask();
+    })
+
 
 
 }); 
